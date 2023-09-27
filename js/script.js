b0df1429d5233d5cc7380a77d46456a27c38633f
Risat <113694977+sakibulkabir@users.noreply.github.com>
2023-09-27 14:20:26 +0600

    changes html,css,js

--- js/script.js
+++ js/script.js
@@ -73,3 +73,17 @@ const header =document.querySelectorAll("header")
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY>50)
 })
+
+///////////////// toggle icon
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle("bx-x")
+    navlist.classList.toggle("open")
+}
+
+window.onscroll =()=>{
+    menuIcon.classList.remove("bx-x")
+    navlist.classList.remove("open")
+}
+
